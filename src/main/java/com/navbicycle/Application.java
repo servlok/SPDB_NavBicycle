1109e969079adc1ea181696c061f1a538aa6879e
WezSieTato <le0n.uo@gmail.com>
2016-01-08 00:46:11 +0100

    DistanceMatrixProvider

--- src/main/java/com/navbicycle/Application.java
+++ src/main/java/com/navbicycle/Application.java
@@ -1,8 +1,12 @@
 package com.navbicycle;
 
+import com.navbicycle.osrm.DistanceMatrixProvider;
+import com.navbicycle.osrm.model.Coordinate;
+import com.navbicycle.osrm.model.RouterResponse;
 import com.navbicycle.veturilo.PlaceProvider;
 import com.navbicycle.veturilo.model.Place;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -13,6 +17,14 @@ public class Application {
         PlaceProvider placeProvider = new PlaceProvider();
         List<Place> places = placeProvider.downloadPlaces();
         System.out.println(places.size());
+
+        List<Coordinate> coordinates = new ArrayList<Coordinate>(2);
+        coordinates.add(new Coordinate(52.230165, 21.012618));
+        coordinates.add(new Coordinate(52.231436, 21.021379));
+
+        DistanceMatrixProvider matrixProvider = new DistanceMatrixProvider();
+        RouterResponse routerResponse = matrixProvider.downloadMatrix(coordinates);
+        System.out.println(routerResponse);
 
     }
 }
--- src/main/java/com/navbicycle/osrm/DistanceMatrixProvider.java
+++ src/main/java/com/navbicycle/osrm/DistanceMatrixProvider.java
@@ -1,7 +1,51 @@
 package com.navbicycle.osrm;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.navbicycle.osrm.model.Coordinate;
+import com.navbicycle.osrm.model.RouterResponse;
+import com.oracle.tools.packager.IOUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by marcinstepnowski on 07.01.2016.
  */
 public class DistanceMatrixProvider {
+
+    public RouterResponse downloadMatrix(List<Coordinate> coordinates) {
+        URI uri = QueryConstructor.uriWithCoordinates(coordinates);
+        String string;
+
+        try {
+            URL url = uri.toURL();
+            URLConnection urlConnection = url.openConnection();
+            InputStream is = urlConnection.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+
+            int numCharsRead;
+            char[] charArray = new char[1024];
+            StringBuffer sb = new StringBuffer();
+            while ((numCharsRead = isr.read(charArray)) > 0) {
+                sb.append(charArray, 0, numCharsRead);
+            }
+            string = sb.toString();
+
+
+        } catch (MalformedURLException e) {
+            return null;
+        } catch (IOException e) {
+            return null;
+        }
+
+        return ResponseReader.read(string);
+    }
 }
